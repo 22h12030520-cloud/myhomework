6b59a05d2111547ed03f440b3ce1cfdb1921b4b5
22h <259055195+22h12030520-cloud@users.noreply.github.com>
2026-04-10 14:15:23 +0800

    init

---
+++ Config.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+#  记得这里要改成你自己的地址！！！！！！！！！
+# 图片及视频检测结果保存路径
+save_path = 'D:/yaohejiance/save_data/'
+
+# 使用的模型路径
+model_path = 'D:/yaohejiance/models/best.pt'
+
+
+names = {
+    0: 'carton box', 1:'cracked carton box', 2:'opened carton box', 3:'wet carton box'
+}
+
+CH_names = [
+    "纸箱",
+    "破损的纸箱",
+    "打开的纸箱",
+    "潮湿的纸箱"
+
+]
